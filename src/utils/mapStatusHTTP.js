2f6bf99d7b699028eadf7fb31a8dbe92c9275697
EvelynBastos <lynbastos@icloud.com>
2024-01-30 01:03:57 -0300

    feat: mapStatus

---
+++ src/utils/mapStatusHTTP.js
@@ -0,0 +1,13 @@
+const httpStatusMap = {
+  SUCCESS: 200,
+  CREATED: 201,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INVALID_VALUE: 422,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+};
+
+const mapStatusHTTP = (status) => httpStatusMap[status] || 500;
+
+export default mapStatusHTTP;
